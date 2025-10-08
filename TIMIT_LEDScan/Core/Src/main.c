e2d3ad1453081241c2b746fff1a7cc7ce378ba4f
Huy <soaicazed1994@gmail.com>
2025-10-08 17:17:06 +0700

    changes

--- TIMIT_LEDScan/Core/Src/main.c
+++ TIMIT_LEDScan/Core/Src/main.c
@@ -119,10 +119,10 @@ int main(void)
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
   software_timer_init();
-  uint8_t intr0 = setTimer(0, 0); 		/* timer for update real-time */
-  uint8_t intr1 = setTimer(0, 0); 		/* timer for 4-led digital clock scanning frequency */
-  uint8_t intr2 = setTimer(0, 10); 		/* timer for scanning speed, used for display LED matrix */
-  uint8_t intr3 = setTimer(1000, 1000); 		/* timer for animations, define how fast is the transition */
+  uint8_t intr0 = setTimer(0, 1000); 		/* timer for update real-time */
+  uint8_t intr1 = setTimer(250, 250); 		/* timer for 4-led digital clock scanning frequency */
+  uint8_t intr2 = setTimer(0, 0); 		/* timer for scanning speed, used for display LED matrix */
+  uint8_t intr3 = setTimer(0, 0); 		/* timer for animations, define how fast is the transition */
   int8_t row_led = LED_MATRIX_ROW - 1;
   int8_t led_index = 3;
   init_frame(charA);
@@ -144,6 +144,7 @@ int main(void)
 	  			  clear_flag();
 	  			  HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
 	  			  HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+	  			  updateClockBuffer();
 
 	  			  second++;
 	  			  if (second >= 60)
@@ -160,7 +161,7 @@ int main(void)
 	  			  {
 	  				  hour = 0;
 	  			  }
-	  			  updateClockBuffer();
+
 	  			  break;
 	  		  }
 	  		  if (flag == intr1)
@@ -188,7 +189,7 @@ int main(void)
 	  		  {
 	  			  clear_flag();
 	  			  shift_left(1);
-	  			  shift_up(1);
+	  			  //shift_up(1);
 	  			  break;
 	  		  }
 	  }
